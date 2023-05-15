c05be62deecd2032090ca04559928aa93e29396c
johnobodai <j.obodai@alustudent.com>
2023-05-15 21:46:56 +0100

    updated

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,14 @@
-# Testing how well the web server setup featuring Nginx is doing under pressure
-exec { 'replace_limit':
-  path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx; service nginx restart',
+# increase ULIMTI
+
+# declare nginx service and make sure it is running
+service { 'nginx':
+  ensure => running,
+}
+
+exec { 'increase ULIMIT':
+  # replace with new ULIMIT value
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/g" /etc/default/nginx',
+  provider => 'shell',
+  # tells nginx to restart
+  notify   => Service['nginx']
 }
